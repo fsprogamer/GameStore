c777907c6aff79720b6a94db24c8164afa331d38
fsprogamer <fsprogamer@gmail.com>
2016-10-04 18:01:08 +0300

    Cart+

---
+++ GameStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGameRepository repository;
+        public CartController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                GetCart().AddItem(game, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                GetCart().RemoveLine(game);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
